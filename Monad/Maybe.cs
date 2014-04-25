2d3bbb051781f95c886c39934f944c872fbd00a6
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-04-25 18:51:02 -0300

    Monad: Suporte a funcoes genericas
    Suprote ao padrao Maybe e a funcao Serialize e Deserialize para
    uso em todo o Framework.

---
+++ Monad/Maybe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AddOne.Framework.Monad
+{
+    // based on http://devtalk.net/csharp/chained-null-checks-and-the-maybe-monad/
+    public static class MaybeClass
+    {
+        public static V With<T, V>(this T t, Func<T, V> selector)
+            where T : class
+            where V : class
+        {
+            if (t == null) return null;
+            return selector(t);
+        }
+
+        public static TResult Return<TInput,TResult>(this TInput o, Func<TInput, TResult> evaluator, TResult failureValue) 
+            where TInput: class
+        {
+            if (o == null) return failureValue;
+            return evaluator(o);
+        }
+
+        public static TInput If<TInput>(this TInput o, Func<TInput, bool> evaluator)
+            where TInput : class
+        {
+            if (o == null) return null;
+            return evaluator(o) ? o : null;
+        }
+
+        public static TInput Unless<TInput>(this TInput o, Func<TInput, bool> evaluator)
+            where TInput : class
+        {
+            if (o == null) return null;
+            return evaluator(o) ? null : o;
+        }
+
+        public static TInput Do<TInput>(this TInput o, Action<TInput> action)
+            where TInput : class
+        {
+            if (o == null) return null;
+            action(o);
+            return o;
+        }
+    }
+}
---
+++ Monad/XML.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+
+namespace AddOne.Framework.Monad
+{
+    internal class MyXmlTextWriter : XmlTextWriter
+    {
+        public MyXmlTextWriter(Stream stream)
+            : base(stream, Encoding.Unicode)
+        {
+
+        }
+
+        public override void WriteEndElement()
+        {
+            base.WriteFullEndElement();
+        }
+    }
+
+    public static class XMLClass
+    {
+        public static string Serialize<T>(this T obj)
+            where T : class
+        {
+            if (obj == null)
+                return null;
+
+            var listSerializer = new XmlSerializer(typeof(T));
+            var xnameSpace = new XmlSerializerNamespaces();
+            xnameSpace.Add("", "");
+            var stream = new MemoryStream();
+            var writer = new MyXmlTextWriter(stream);
+            listSerializer.Serialize(writer, obj, xnameSpace);
+            return Encoding.Unicode.GetString(stream.ToArray());
+        }
+
+        public static V Deserialize<V>(this string xml)
+            where V : class
+        {
+            var objSerializer = new XmlSerializer(typeof(V), "");
+            return (V)objSerializer.Deserialize(GenerateStreamFromString(xml));
+        }
+
+        private static Stream GenerateStreamFromString(string s)
+        {
+            // wront UTF-16 return from SAP.
+            // TODO: testar tratamento utilizando no Serialize<T>.
+            if (s.ToUpper().StartsWith("<?xml version=\"1.0\" encoding=\"UTF-16\"?>".ToUpper()))
+                s = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + s.Substring(39);
+
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+    }
+}
