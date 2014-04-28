4f85e1ff396c985b70c73a847eaf09b1a8cb5d30
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-04-28 12:37:11 -0300

    MenuAttribute: Adicao de suporte a i18n.
    Suporte a i18n adicionado, com campo i18n que recebe a chave
    do Resource de internacionalizacao onde o texto esta armazenado.

--- Attribute/MenuAttribute.cs
+++ Attribute/MenuAttribute.cs
@@ -2,27 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SAPbouiCOM;
 
 namespace AddOne.Framework.Attribute
 {
-    public enum MenuType
-    {
-        Folder = 2,
-        Item = 1
-    }
 
-
-    public class MenuAttribute : System.Attribute
+    [System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]
+    public class MenuAttribute : System.Attribute, IComparable<MenuAttribute>
     {
-        public string Checked;
-        public string Enabled;
+        public string Checked = "0";
+        public string Enabled = "1";
         public string FatherUID;
         public string String;
-        public MenuType Type;
+        public BoMenuType Type;
         public string UniqueID;
         public string Image;
+        public string ValidateMethod;
+        public string i18n;
+        public int Order;
 
-        public MenuAttribute(string FatherUID, string String, MenuType Type, string UniqueID,
+        public MenuAttribute() { }
+
+        public MenuAttribute(string FatherUID, string String, BoMenuType Type, string UniqueID,
             string Image = null, string Checked = "0", string Enabled = "1")
         {
             this.Checked = Checked;
@@ -34,5 +35,12 @@ namespace AddOne.Framework.Attribute
             this.Image = Image;
         }
 
+
+        internal System.Type OriginalType { get; set; }
+
+        public int CompareTo(MenuAttribute other)
+        {
+            return this.Order.CompareTo(other.Order);
+        }
     }
 }
