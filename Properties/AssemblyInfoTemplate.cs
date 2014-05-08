3b9f18880dad0151e0d62cb83645a33e06ac122b
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-05-08 11:19:32 -0300

    Template para versionamento automatico

---
+++ Properties/AssemblyInfoTemplate.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Framework")]
+[assembly: AssemblyDescription("AddOne Framework - BRANCH - FULLTAG")]
+[assembly: AssemblyConfiguration("BRANCH")]
+[assembly: AssemblyCompany("GWEAdded")]
+[assembly: AssemblyProduct("AddOne")]
+[assembly: AssemblyCopyright("Copyright © GWEAdded 2012/2013/2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b1bf39bb-7830-46b7-9760-abc6cece7cd0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("FULLTAG")]
+[assembly: AssemblyVersion("TAG")]
+[assembly: AssemblyFileVersion("TAG")]
