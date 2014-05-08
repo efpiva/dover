2fe31db0e7f447800fa0fba69c5b6be3919eb4f2
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-05-07 23:34:08 -0300

    StringExtensions: metodos a-la mssql

---
+++ Monad/StringExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AddOne.Framework.Monad
+{
+    public static class StringExtension
+    {
+        public static String Left(this String s, Int32 length)
+        {
+            if (s.Length <= length)
+                return s;
+
+            return s.Substring(0, length);
+        }
+
+        public static String Right(this String s, Int32 length)
+        {
+            if (s.Length <= length)
+                return s;
+            return s.Substring(s.Length - length, length);
+        }
+
+        public static String Truncate(this String s, Int32 length)
+        {
+            return s.Length > length ? s.Substring(0, length) : s;
+        }
+
+    }
+}
