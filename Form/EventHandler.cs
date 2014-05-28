ad1b8fbd6c436dd20d6c8f596acfe9444ed5e57b
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-05-28 09:20:02 -0300

    EventHandler: Removed Logger and added new delegates for different EventTypes

--- Form/EventHandler.cs
+++ Form/EventHandler.cs
@@ -6,13 +6,92 @@ using System.Reflection;
 using Castle.Core.Logging;
 using SAPbouiCOM.Framework;
 using SAPbouiCOM;
+using AddOne.Framework.Factory;
 
 namespace AddOne.Framework.Form
 {
     public static class EventHandler
     {
 
-        public static _IButtonEvents_ClickBeforeEventHandler ExceptionHandler(this _IButtonEvents_ClickBeforeEventHandler eventTrigger, FormBase form, ILogger Logger)
+        private static ILogger Logger = ContainerManager.Container.Resolve<ILogger>();
+
+        public static _IColumnEvents_ComboSelectBeforeEventHandler ExceptionHandler(this _IColumnEvents_ComboSelectBeforeEventHandler eventTrigger, FormBase form)
+        {
+            _IColumnEvents_ComboSelectBeforeEventHandler retFunction = (object sboObject, SAPbouiCOM.SBOItemEventArg pVal, out bool BubbleEvent) =>
+            {
+                BubbleEvent = true;
+                try
+                {
+                    eventTrigger(sboObject, pVal, out BubbleEvent);
+                }
+                catch (Exception e)
+                {
+                    if (form != null && form.UIAPIRawForm != null)
+                        form.UIAPIRawForm.Freeze(false); // force unfreeze in case of error.
+
+                    Assembly addinAssembly = eventTrigger.Method.DeclaringType.Assembly;
+                    Version objVersion = addinAssembly.GetName().Version;
+                    String addInName = addinAssembly.GetName().Name;
+                    String addInVersion = objVersion.Major.ToString() + "." + objVersion.Minor.ToString() + "." + objVersion.Build.ToString()
+                                + "." + objVersion.Revision;
+
+                    Logger.Error(String.Format(Messages.AddInError, addInName, addInVersion), e);
+                }
+            };
+            return retFunction;
+        }
+
+        public static _IGridEvents_PressedAfterEventHandler ExceptionHandler(this _IGridEvents_PressedAfterEventHandler eventTrigger, FormBase form)
+        {
+            _IGridEvents_PressedAfterEventHandler retFunction = (object sboObject, SAPbouiCOM.SBOItemEventArg pVal) =>
+            {
+                try
+                {
+                    eventTrigger(sboObject, pVal);
+                }
+                catch (Exception e)
+                {
+                    if (form != null && form.UIAPIRawForm != null)
+                        form.UIAPIRawForm.Freeze(false); // force unfreeze in case of error.
+
+                    Assembly addinAssembly = eventTrigger.Method.DeclaringType.Assembly;
+                    Version objVersion = addinAssembly.GetName().Version;
+                    String addInName = addinAssembly.GetName().Name;
+                    String addInVersion = objVersion.Major.ToString() + "." + objVersion.Minor.ToString() + "." + objVersion.Build.ToString()
+                                + "." + objVersion.Revision;
+
+                    Logger.Error(String.Format(Messages.AddInError, addInName, addInVersion), e);
+                }
+            };
+            return retFunction;
+        }
+
+        public static _IColumnEvents_ComboSelectAfterEventHandler ExceptionHandler(this _IColumnEvents_ComboSelectAfterEventHandler eventTrigger, FormBase form)
+        {
+            _IColumnEvents_ComboSelectAfterEventHandler retFunction = (object sboObject, SAPbouiCOM.SBOItemEventArg pVal) =>
+            {
+                try
+                {
+                    eventTrigger(sboObject, pVal);
+                }
+                catch (Exception e)
+                {
+                    if (form != null && form.UIAPIRawForm != null)
+                        form.UIAPIRawForm.Freeze(false); // force unfreeze in case of error.
+
+                    Assembly addinAssembly = eventTrigger.Method.DeclaringType.Assembly;
+                    Version objVersion = addinAssembly.GetName().Version;
+                    String addInName = addinAssembly.GetName().Name;
+                    String addInVersion = objVersion.Major.ToString() + "." + objVersion.Minor.ToString() + "." + objVersion.Build.ToString()
+                                + "." + objVersion.Revision;
+
+                    Logger.Error(String.Format(Messages.AddInError, addInName, addInVersion), e);
+                }
+            };
+            return retFunction;
+        }
+
+        public static _IButtonEvents_ClickBeforeEventHandler ExceptionHandler(this _IButtonEvents_ClickBeforeEventHandler eventTrigger, FormBase form)
         {
             _IButtonEvents_ClickBeforeEventHandler retFunction = (object sboObject, SAPbouiCOM.SBOItemEventArg pVal, out bool BubbleEvent) =>
             {
@@ -30,7 +109,7 @@ namespace AddOne.Framework.Form
                     Version objVersion = addinAssembly.GetName().Version;
                     String addInName = addinAssembly.GetName().Name;
                     String addInVersion = objVersion.Major.ToString() + "." + objVersion.Minor.ToString() + "." + objVersion.Build.ToString()
-                + "." + objVersion.Revision;
+                                + "." + objVersion.Revision;
 
                     Logger.Error(String.Format(Messages.AddInError,addInName, addInVersion), e);                    
                 }
@@ -40,7 +119,7 @@ namespace AddOne.Framework.Form
             return retFunction;
         }
 
-        public static _IButtonComboEvents_ClickAfterEventHandler ExceptionHandler(this _IButtonComboEvents_ClickAfterEventHandler eventTrigger, FormBase form, ILogger Logger)
+        public static _IButtonComboEvents_ClickAfterEventHandler ExceptionHandler(this _IButtonComboEvents_ClickAfterEventHandler eventTrigger, FormBase form)
         {
             _IButtonComboEvents_ClickAfterEventHandler retFunction = (object sboObject, SAPbouiCOM.SBOItemEventArg pVal) =>
             {
@@ -67,7 +146,7 @@ namespace AddOne.Framework.Form
             return retFunction;
         }
 
-        public static _IButtonComboEvents_ClickAfterEventHandler ChooseFromListHandler(this SAPbouiCOM.EditText targetEdit, SAPbouiCOM.UserDataSource ds, ILogger Logger)
+        public static _IButtonComboEvents_ClickAfterEventHandler ChooseFromListHandler(this SAPbouiCOM.EditText targetEdit, SAPbouiCOM.UserDataSource ds)
         {
             _IButtonComboEvents_ClickAfterEventHandler handler = (object sboObject, SAPbouiCOM.SBOItemEventArg pVal) =>
             {
@@ -80,7 +159,7 @@ namespace AddOne.Framework.Form
 
             };
 
-            return handler.ExceptionHandler(null, Logger);
+            return handler.ExceptionHandler(null);
         }
     }
 }
