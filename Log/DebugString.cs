6d0555fabb71458ca104a576676e90d8ea9ac59b
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-06-19 22:13:24 -0300

    DebugString: Created Func<string> messageFactory
    This messageFactory prevents string format to be called when
    applications is not in debug mode, using less memory
    and increasing performance.

---
+++ Log/DebugString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AddOne.Framework.Log
+{
+    public static class DebugString
+    {
+        public static Func<string> Format(String msg, params object[] values)
+        {
+            Func<string> debugFunc = () =>
+            {
+                if (values == null || values.Length == 0)
+                    return msg;
+
+                return String.Format(msg, values);
+            };
+            return debugFunc;
+        }
+    }
+}
