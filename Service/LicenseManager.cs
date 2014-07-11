fe554a8e98c36d1d1194bd136428756ec50173c1
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-07-11 17:51:12 -0300

    Stub license implementation

---
+++ Service/LicenseManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dover.Framework.Model;
+using Dover.Framework.DAO;
+
+namespace Dover.Framework.Service
+{
+    /// <summary>
+    /// Stub class
+    /// </summary>
+    public class LicenseManager
+    {
+        private AssemblyDAO asmDAO;
+
+        public LicenseManager(AssemblyDAO asmDAO)
+        {
+            this.asmDAO = asmDAO;
+        }
+
+        internal List<AssemblyInformation> ListAddins()
+        {
+            return asmDAO.getAssembliesInformation("A");
+        }
+
+        internal void BootLicense()
+        {
+            // fake implementation
+        }
+
+        public void SaveLicense(string p)
+        {
+            // fake implementation
+        }
+
+        public DateTime GetAddInExpireDate(string module)
+        {
+            return DateTime.MaxValue;
+        }
+    }
+}
