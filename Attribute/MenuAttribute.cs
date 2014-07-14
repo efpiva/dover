0e27d255e16074e8c15011ac14c3f2baaf6aca10
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-07-13 23:51:27 -0300

    MenuAttribute: Documentation and minor refactoring
    Added documentation and changed Order name for Position, to match
    SDK standard.
    
    Also added a default empty string for Image, to prevent null pointer.

--- Attribute/MenuAttribute.cs
+++ Attribute/MenuAttribute.cs
@@ -10,19 +10,73 @@ namespace Dover.Framework.Attribute
     [System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]
     public class MenuAttribute : System.Attribute, IComparable<MenuAttribute>
     {
+        /// <summary>
+        /// Indicates whether the menu item gets a check mark when clicked.
+        /// </summary>
         public string Checked = "0";
+        /// <summary>
+        /// Indicates whether the menu item is enabled.
+        /// </summary>
         public string Enabled = "1";
+        /// <summary>
+        /// UID of the top level menu
+        /// </summary>
         public string FatherUID;
+        /// <summary>
+        /// The display name of the menu item.
+        /// </summary>
         public string String;
+        /// <summary>
+        /// The type of menu item.
+        /// mt_STRING - active menu item with no subitems (menu ID should be greater than 0)
+        /// mt_POPUP - Menu item with subitems
+        /// mt_SEPERATOR - Separator item
+        /// </summary>
         public BoMenuType Type;
+        /// <summary>
+        /// The unique ID of the menu item.
+        /// </summary>
         public string UniqueID;
-        public string Image;
+        /// <summary>
+        /// The path to an image file to be displayed to the left of the menu item.
+        /// </summary>
+        public string Image = "";
+        /// <summary>
+        /// The name of a method, in the current class, that will indicates at runtime if the menu should be
+        /// enabled or disabled. This method can, for example, check user permissions or check if user is super-user.
+        /// 
+        /// the method should return bool and have no parameters, so as an example:
+        /// 
+        /// public bool MyMethod();
+        /// 
+        /// This parameter would be: MyMethod.
+        /// </summary>
         public string ValidateMethod;
+        /// <summary>
+        /// A resource full qualified name to be used as menu String.
+        /// </summary>
         public string i18n;
-        public int Order;
+        /// <summary>
+        /// The position within the menu to place the menu item (1-based).
+        /// </summary>
+        public int Position;
 
         public MenuAttribute() { }
 
+        /// <summary>
+        /// Add a Menu Item to SAP Bussiness One Menus.
+        /// </summary>
+        /// <param name="FatherUID">UID of the top level menu</param>
+        /// <param name="String">The display name of the menu item.</param>
+        /// <param name="Type">The type of menu item.
+        /// mt_STRING - active menu item with no subitems (menu ID should be greater than 0)
+        /// mt_POPUP - Menu item with subitems
+        /// mt_SEPERATOR - Separator item
+        /// </param>
+        /// <param name="UniqueID">The unique ID of the menu item.</param>
+        /// <param name="Image">The path to an image file to be displayed to the left of the menu item.</param>
+        /// <param name="Checked">Indicates whether the menu item gets a check mark when clicked.</param>
+        /// <param name="Enabled">Indicates whether the menu item is enabled.</param>
         public MenuAttribute(string FatherUID, string String, BoMenuType Type, string UniqueID,
             string Image = null, string Checked = "0", string Enabled = "1")
         {
@@ -40,7 +94,7 @@ namespace Dover.Framework.Attribute
 
         public int CompareTo(MenuAttribute other)
         {
-            return this.Order.CompareTo(other.Order);
+            return this.Position.CompareTo(other.Position);
         }
     }
 }
