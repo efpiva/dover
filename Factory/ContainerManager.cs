082dba7b3bbb6ba554c41c66d08552b4983351f5
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-07-13 23:57:05 -0300

    Container Factory documentation and Framework form registration support.
    Added documentation and registered form proxy not only on addins, but on Framework also.
    
    Did also some refactoring, registering just DAOs, Service and needed classes, reducing
    footprint.

--- Factory/ContainerManager.cs
+++ Factory/ContainerManager.cs
@@ -18,15 +18,27 @@ using Dover.Framework.Form;
 
 namespace Dover.Framework.Factory
 {
-
+    /// <summary>
+    /// Class that encapsulates creation of WindsorContainer, responsible for IoC of Dover framework.
+    /// 
+    /// </summary>
     public class ContainerManager
     {
+        /// <summary>
+        /// If you want to run Dover Framework as a windows service or as a command line tool, 
+        /// one or more company Factors should be defined, since there will be no UI-API
+        /// to do the connection using session authentication.
+        /// </summary>
         public static Func<SAPbobsCOM.Company>[] customCompanyFactory {get; set; }
+        /// <summary>
+        /// WindsorContainer used by Dover. In some situations you may need to manipulate it directly.
+        /// 
+        /// Use with caution.
+        /// </summary>
         public static IWindsorContainer Container { get; private set; }
 
         internal static void RegisterAssembly(Assembly addIn)
         {
-            Container.Register(Component.For<IInterceptor>().ImplementedBy<FormProxy>().Named("formProxy"));
             Container.Register(Classes.FromAssembly(addIn)
                             .IncludeNonPublicTypes().Pick()
                             .WithService.DefaultInterfaces().LifestyleTransient());
@@ -41,13 +53,18 @@ namespace Dover.Framework.Factory
 
             Container = new WindsorContainer();
             Container.Kernel.Resolver.AddSubResolver(new ArrayResolver(Container.Kernel));
-            Container.Register(Component.For<LicenseDAO>().ImplementedBy<LicenseDAOSQLImpl>());
+            // proxy for forms.
+            Container.Register(Component.For<IInterceptor>().ImplementedBy<FormProxy>().Named("formProxy"));
+            // forms are Transient.
+            Container.Register(Classes.FromThisAssembly().IncludeNonPublicTypes().InNamespace("Dover.Framework.Form")
+                .WithService.DefaultInterfaces().LifestyleTransient());
 
+            // SAP App Facttory.
             Container.Register(Component.For<SAPbouiCOM.Application>().UsingFactoryMethod(SAPServiceFactory.ApplicationFactory));
 
+            // company factory (use default, that initializes DI API from App if none was provided).
             for(int i=0 ; i< companyFactory.Length ; i++)
                 Container.Register(Component.For<SAPbobsCOM.Company>().UsingFactoryMethod(companyFactory[i]).Named("company" + i));
-
             for (int i = 0; i < companyFactory.Length; i++)
                 Container.Register(Component.For<BusinessOneDAO>().ImplementedBy<BusinessOneDAOSQLImpl>()
                     .DependsOn(Dependency.OnComponent(typeof(SAPbobsCOM.Company), "company" + i)).Named("b1dao" + i));
@@ -55,12 +72,22 @@ namespace Dover.Framework.Factory
 
             string runningFolder = Path.GetDirectoryName( AppDomain.CurrentDomain.BaseDirectory );
 
-            Container.Register(Classes.FromThisAssembly().IncludeNonPublicTypes().Pick()
+            // Service and DAO registration, they are singleton.
+            Container.Register(Classes.FromThisAssembly().IncludeNonPublicTypes().InNamespace("Dover.Framework.Service")
                 .WithService.DefaultInterfaces().LifestyleSingleton());
+            Container.Register(Classes.FromThisAssembly().IncludeNonPublicTypes().InNamespace("Dover.Framework.DAO")
+                .WithService.DefaultInterfaces().LifestyleSingleton());
+
+            // Core and MicroCore
+            Container.Register(Component.For<MicroCore>().LifestyleSingleton());
+            Container.Register(Component.For<MicroBoot>().LifestyleSingleton());
+            Container.Register(Component.For<Boot>().LifestyleSingleton());
 
             assemblyName = Assembly.GetEntryAssembly() == null ? (string)AppDomain.CurrentDomain.GetData("assemblyName") 
                 : Assembly.GetEntryAssembly().GetName().Name;
 
+            if (assemblyName == "Framework")
+                assemblyName = "Dover"; // Framework should be threated the same as Dover.
             Container.AddFacility<LoggingFacility>(f => f.UseLog4Net(assemblyName + ".config"));
 
             var logger = Container.Resolve<ILogger>();
