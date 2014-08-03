279eaeca4cf3c03411f60848f21c8f34134c0255
Eduardo Piva <efpiva@gmail.com>
2014-08-03 16:55:27 -0300

    Log/SAPAppender: Move SQL to resources. Support for HANA and SQL

--- Log/SAPAppender.cs
+++ Log/SAPAppender.cs
@@ -71,8 +71,7 @@ namespace Dover.Framework.Log
 
         private void DIAPILog(LoggingEvent loggingEvent, string asm, string version)
         {
-            string sqlTemplate = @"INSERT INTO [@DOVER_LOGS] (Code, Name, U_Date, U_Hour, U_User, U_MachineName, U_MacAddress, U_IP,
-                U_Version, U_Assembly) Values ('{0}', '{1}', '{2}', {3}, '{4}', '{5}', '{6}', '{7}', '{8}', '{9}')";
+            string sqlTemplate = this.GetSQL("InsertLog.sql");
 
             string code = B1DAO.GetNextCode("DOVER_LOGS");
             DateTime date = DateTime.Now;
---
+++ Log/hana/InsertLog.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO "@DOVER_LOGS" ("Code", "Name", "U_Date", "U_Hour", "U_User", "U_MachineName", "U_MacAddress", U_IP, "U_Version", "U_Assembly")
+VALUES ('{0}', '{1}', '{2}', {3}, '{4}', '{5}', '{6}', '{7}', '{8}', '{9}')
