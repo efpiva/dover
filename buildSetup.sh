0c3e99a011b6712aa34ac5ef1a6919cea89ff551
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-08-06 00:51:11 -0300

    Automated ARD: buildSetup.sh script that automatically creates ARD and Setup with correct version

---
+++ buildSetup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+export fulltag=$(git describe --tags)
+export tag=$(echo $fulltag | sed s/-/./ | sed s/-.*//)
+
+MSBuild.exe AddOnInstallAPI/AddOnInstallAPI.sln //p:Configuration=Release
+MSBuild.exe Dover.sln //p:Configuration=Release //p:Platform=x64
+
+cp AddOnInstallAPI/bin/Release/AddOnInstallAPI.exe Core/bin/x64/Release
+cp Install/Doverx64.iss Core/bin/x64/Release
+pushd .
+cd Core/bin/x64/Release
+iscc Doverx64.iss
+popd
+
+echo "Creating ard file for $fulltag on branch $branch - git_branch $GIT_BRANCH"
+
+rm -rf build
+
+export setup_dir=build/x64/setup
+export exe_dir=build/x64/exe
+export dll_dir=build/x64/dll
+
+mkdir -p $exe_dir
+mkdir -p $dll_dir
+mkdir -p $setup_dir
+
+cp -a Core/bin/x64/Release/*.exe $exe_dir
+cp -a Core/bin/x64/Release/*.dll $dll_dir
+cp -a Core/bin/x64/Release/Output/setup.exe $setup_dir
+
+rm -rf temp
+mkdir temp
+cp Install/Doverx64.xml temp
+cp $setup_dir/setup.exe temp
+cp $exe_dir/Dover.exe temp
+
+cd temp
+AddOnRegDataGen Doverx64.xml $tag setup.exe setup.exe Dover.exe
+cd ..
+
+mv temp/Dover.ard $setup_dir
+
+# 32 bits stuff
+MSBuild.exe Dover.sln //p:Configuration=Release //p:Platform=x86
+
+cp Install/Dover.iss Core/bin/Release
+pushd .
+cd Core/bin/Release
+iscc Dover.iss
+popd
+
+export setup_dir=build/x86/setup
+export exe_dir=build/x86/exe
+export dll_dir=build/x86/dll
+
+mkdir -p $exe_dir
+mkdir -p $setup_dir
+mkdir -p $dll_dir
+
+cp -a Core/bin/Release/*.exe $exe_dir
+cp -a Core/bin/Release/*.dll $dll_dir
+cp -a Core/bin/Release/Output/setup.exe $setup_dir
+
+rm -rf temp
+mkdir temp
+cp Install/Dover.xml temp
+cp $setup_dir/setup.exe temp
+cp $exe_dir/Dover.exe temp
+
+cd temp
+AddOnRegDataGen Dover.xml $tag setup.exe setup.exe Dover.exe
+cd ..
+
+mv temp/Dover.ard $setup_dir
