a1019109804efef81c7fe740212eeeac12aeba76
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-08-29 18:13:45 -0300

    Application: Included name in AddIn annotation

--- Application.cs
+++ Application.cs
@@ -48,7 +48,7 @@ namespace Dover.Framework
     /// Dover.Framework.Container.ContainerManager.
     /// 
     /// </summary>
-    [AddIn(i18n="Dover.Framework.Messages.DoverName")]
+    [AddIn(i18n="Dover.Framework.Messages.DoverName", Description="Dover Framework")]
     [ResourceBOM("Dover.Framework.DatabaseTables.xml", ResourceType.UserTable)]
     [ResourceBOM("Dover.Framework.DatabaseFields.xml", ResourceType.UserField)]
     public class Application : MarshalByRefObject
