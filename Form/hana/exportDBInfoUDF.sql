33f17308add53685eb70ab16b7eb1b1f216050f5
Eduardo Piva <efpiva@gmail.com>
2015-03-19 09:40:39 -0300

    ExportDB: Bug on datatables size.
    Casted all sql form ExportDB to the same size, so when loading/exporting
    it doesn't overflow.
    
    Haven't tested the HANA version, no HANA here. :(

--- Form/hana/exportDBInfoUDF.sql
+++ Form/hana/exportDBInfoUDF.sql
@@ -1,3 +1,3 @@
-﻿SELECT "TableID" AS "Id1", CAST("FieldID" AS nvarchar) AS "Id2", '[' || "TableID" || '].' || "AliasID" AS "Code", 
-    "Descr" AS "Name" 
+﻿SELECT "TableID" AS "Id1", CAST("FieldID" AS nvarchar) AS "Id2", cast('[' || "TableID" || '].' || "AliasID" AS nvarchar(20)) AS "Code", 
+    cast("Descr" as nvarchar(100)) AS "Name" 
 FROM CUFD
