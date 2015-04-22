f0fbae13497a9ef966ed1596e65d3fac75d903d1
Eduardo Piva <efpiva@gmail.com>
2015-04-22 10:20:49 -0300

    missing files. :-/

---
+++ FrameworkInterface/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Castle.Core.Internal;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: InternalsVisibleTo(InternalsVisible.ToDynamicProxyGenAssembly2)]
+[assembly: InternalsVisibleTo("Framework, PublicKey=002400000480000094000000060200000024000052534131000400000100010057015eb62f506d249895c9324b3dbe9b4274322a76cee903681e7855baebddc5a1633dadbd4471999a8486dde8f33e17dbf2ec7604a669bfa7ec9c367e046befd4be6b535b459d11ef6f7e393a7dd1acb0ae8a397e9637d1ba7240cad85da811bc5c35b5b8d6e79a18c05f64747490dbc9bb76cf697214b168079f63211857db")]
+[assembly: AssemblyTitle("FrameworkInterface")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FrameworkInterface")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("68c8f76a-7a75-41eb-bd81-70bacbee0956")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
